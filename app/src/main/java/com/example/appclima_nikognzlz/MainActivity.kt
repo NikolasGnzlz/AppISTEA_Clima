4833880c5687c4542166eff8c6035fb6441b7fb4
NikolasGnzlz <nikolasgnzlz@gmail.com>
2024-10-29 19:14:05 -0300

    Commit inicial: 2do Parcial App del Clima

---
+++ app/src/main/java/com/example/appclima_nikognzlz/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.appclima_nikognzlz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appclima_nikognzlz.ui.theme.AppClima_NikoGnzlzTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppClima_NikoGnzlzTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppClima_NikoGnzlzTheme {
+        Greeting("Android")
+    }
+}
